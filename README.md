94b2d2906fa0c2debe77a24e5a5bbcd74488c88f
arthur hauser <arthur.hauser@gmail.com>
2020-06-06 19:19:17 -0400

    saturday upload

---
+++ README.md
@@ -0,0 +1,139 @@
+## Unit 10 Homework: Template Engine - Employee Summary
+
+
+### Table of Contents
+
+* [Project Summary](#summary)
+* [Installation [n/a]](#installation)
+* [Usage [n/a]](#usage)
+* [Credits [n/a]](#credits)
+* [License [n/a]](#license)
+* [Acceptance Criteria & Assignment Objectives](#checklist)
+* [Badges [n/a]](#badges)
+* [Contributing](cContributing)
+* [Tests [n/a]](#tests)
+----
+#### Project Summary
+
+
+
+### User input
+
+The project must prompt the user to build an engineering team. An engineering
+team consists of a manager, and any number of engineers and interns.
+
+----
+#### Installation
+n/a
+#### Usage 
+n/a
+----
+#### Credits
+
+1. Solo project
+
+2. Tools/resources used: 
+  - [Bootstrap][1]
+  - [Openweathermap.org][2]
+  - [Jquery.com][3] 
+  - [Inquirer npm package][4]
+  - [Jest.JS][5]
+
+3.	Tutorials used: 
+  - [MDN AJAX: Getting Started][4]
+  - [Ajax Tutorial][5] 
+  - [Jquery - Events][6]
+  - [Jquery - DOM manipulation][7]
+  - [Steps for an AJAX Operation][8]
+
+#### License
+
+n/a
+
+----
+
+#### Homework Acceptance Criteria Assignment Objectives
+###### feature description
+###### checklist status
+###### issues (if applicable)
+
+* [1. Command-Line-App-Team-Generator](#heading)
+* [2. Must-Pass-All-Unit-Tests](#heading-1)
+* [3. Manager's Tool](#heading-2)
+* [4. Require Inquirer](#heading-3)
+* [5. Node CLI](#heading-4)
+* [6. Team Roster](#heading-5)
+
+##### 1. Command-Line-App-Team-Generator
+
+###### feature description: You will build a software engineering team generator command line application. The application will prompt the user for information about the team manager and then information about the team members.  The user can input any number of team members, and they may be a mix of engineers and interns.
+- [x] complete
+
+##### 2. Must-Pass-All-Unit-Tests
+
+###### feature description: This assignment must also pass all unit tests. When the user has completed building the team, the application will create an HTML file that displays a nicely formatted team roster based on the information provided by the user. 
+- [x] complete
+
+##### 3. Manager's Tool
+
+###### feature description: 
+```
+As a manager
+I want to generate a webpage that displays my team's basic info
+so that I have quick access to emails and GitHub profiles
+```
+- [x] complete
+
+##### 4. Require Inquirer
+
+###### feature description: use the [Inquirer npm package](https://github.com/SBoudrias/Inquirer.js/) to prompt the user for their email, id, and specific information based on their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
+- [x] complete
+
+##### 5. Node CLI
+
+###### feature description: your app will run as a Node CLI to gather information about each employee.
+
+- [x] complete
+
+##### 6. Team Roster
+
+###### feature description: The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
+
+  * Name - [x] complete
+
+  * Role - [x] complete
+
+  * ID - [x] complete
+
+  * Role-specific property (School, link to GitHub profile, or office number) 
+- [x] complete
+----
+
+#### Badges
+
+n/a
+
+#### Contributing
+
+[Contributor Covenant](https://www.contributor-covenant.org/)
+
+Employee.test.js
+Engineer.test.js
+Intern.test.js
+Manager.test.js
+
+
+n/a
+###### [link references]
+
+[1]: https://getbootstrap.com/
+[2]: https://openweathermap.org/
+[3]: https://api.jquery.com/
+[4]: https://github.com/SBoudrias/Inquirer.js/
+[5]: https://jestjs.io/
+[4]: https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX/Getting_Started
+[5]: https://www.tutorialspoint.com/ajax/index.htm
+[6]: https://www.tutorialspoint.com/jquery/jquery-events.htm
+[7]: https://www.tutorialspoint.com/jquery/jquery-dom.htm
+[8]: https://www.tutorialspoint.com/ajax/ajax_in_action.htm
+[9]: https://www.contributor-covenant.org/
--- app.js
+++ app.js
@@ -4,32 +4,106 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const app = require("./app");
+
+var employees = [];
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+        
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?",
+        validate: function(value) {
+            var pass = value.match(
+                  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            );
+            if (pass) {
+              return true;
+            }
+      
+            return 'Please enter a valid email address';
+      }
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?"
+    },
+    //ID# check for input is # otherwise repeat question
+    //if (# != indexOf(arrayID)) then proceed otherwise      console.log("That ID # is taken, try another one")
+    {
+        type: "list",
+        name: "role",
+        message: "Is the employee an intern, engineer or manager?",
+        choices: ["intern", "engineer", "manager"]
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "what school did they go to?",
+        when: (answers) => answers.role === "intern"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "what is their github username?",
+        when: (answers) => answers.role === "engineer"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "what is their office number?",
+        when: (answers) => answers.role === "manager"
+    },
+    {
+        type: "confirm",
+        name: "reRun",
+        message: "Do you want to add a new employee?  If not then your roster of employees will be rendered!"
+    }
+]
+
+function addEmployee() {
+
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log(answers);
+            //add answers to the employees array
+            if (answers.role === "intern") {
+                var newIntern = new Intern(answers.name, answers.id, answers.email, answers.school)
+                employees.push(newIntern)
+            };
+            if (answers.role === "engineer") {
+                var newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+                employees.push(newEngineer)
+            };
+            if (answers.role === "manager") {
+                var newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+                employees.push(newManager)
+            };
+
+            if (answers.reRun === true) {
+                addEmployee();
+            } else {
+                fs.writeFile("./output/final.html", render(employees), err => {
+                    if (err) {
+
+                    }
+                    console.log("checkcheck");
+                });
+
+            }
+        })
+}
+
+
+addEmployee();
